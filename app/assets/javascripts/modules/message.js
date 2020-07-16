7a65cdc02f99c494ea3e192639acf611519bd573
Oki-yu <radsolars@yahoo.co.jp>
2020-07-16 15:28:14 +0900

    add s3

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -55,6 +55,7 @@ $(function(){
       contentType: false
     })
     .done(function(data){
+      console.log(data)
       let html = buildHTML(data);
       $('.main-message').append(html);
       $('form')[0].reset();
